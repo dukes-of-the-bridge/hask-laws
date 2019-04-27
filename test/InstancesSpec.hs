e775990cc43896a397b6f9cc8166745e35da53c9
Marc-Daniel Ortega <globulon@gmail.com>
2019-04-27 18:25:56 +0100

    Define pending tests for list seni group algebra

---
+++ test/InstancesSpec.hs
@@ -0,0 +1,12 @@
+module InstancesSpec (spec) where
+
+import Test.Hspec
+
+spec :: Spec
+spec = describe "SemiGroup for Lists" $ do
+  it "close on list addition using length homomorphism" $ do
+    pending
+  it "validate associative laws with grouping" $ do
+    pending
+  it "validate associative laws without grouping" $ do
+    pending
