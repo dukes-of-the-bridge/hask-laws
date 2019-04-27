af829b77251a63624f390a1a2e2684d073089069
Marc-Daniel Ortega <globulon@gmail.com>
2019-04-27 23:18:49 +0100

    Add laws for Monoid

--- test/InstancesSpec.hs
+++ test/InstancesSpec.hs
@@ -6,7 +6,12 @@ import Algebras
 import Instances
 
 spec :: Spec
-spec = describe "SemiGroup for Lists" $ do
+spec = do
+  spec_semigroup
+  spec_monoid
+
+spec_semigroup :: Spec
+spec_semigroup = describe "SemiGroup for Lists" $ do
   it "close on list addition using length homomorphism" $
     property $
       \xs ys -> length (xs::[Int]) |+| length (ys::[Int]) == length (xs |+| ys)
@@ -16,3 +21,13 @@ spec = describe "SemiGroup for Lists" $ do
   it "validate associative laws without grouping" $
     property $
       \xs ys zs-> ((xs::Int) |+| (ys::Int)) |+| (zs::Int) == (xs::Int) |+| (ys::Int) |+| (zs::Int)
+
+spec_monoid :: Spec
+spec_monoid = describe "Monoid for Lists" $ do
+  it "have commutative neutral element" $
+    property $ \xs -> (xs::[Int]) |+| zero == zero |+| xs
+  it "have neutral element" $
+    property $ \xs -> (xs::[Int]) |+| zero == xs
+
+
+
