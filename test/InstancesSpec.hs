d5812f988453a01d21306ab5651dbf205db8ecef
Marc-Daniel Ortega <globulon@gmail.com>
2019-04-28 18:23:52 +0100

    Make laws more visible

--- test/InstancesSpec.hs
+++ test/InstancesSpec.hs
@@ -7,27 +7,35 @@ import Instances
 
 spec :: Spec
 spec = do
-  spec_semigroup
-  spec_monoid
+  spec_list_semigroup
+  spec_list_monoid
 
-spec_semigroup :: Spec
-spec_semigroup = describe "SemiGroup for Lists" $ do
-  it "close on list addition using length homomorphism" $
-    property $
-      \xs ys -> length (xs::[Int]) |+| length (ys::[Int]) == length (xs |+| ys)
-  it "validate associative laws with grouping" $
-    property $
-      \xs ys zs-> ((xs::Int) |+| (ys::Int)) |+| (zs::Int) == (xs::Int) |+| ((ys::Int) |+| (zs::Int))
-  it "validate associative laws without grouping" $
-    property $
-      \xs ys zs-> ((xs::Int) |+| (ys::Int)) |+| (zs::Int) == (xs::Int) |+| (ys::Int) |+| (zs::Int)
+spec_list_semigroup :: Spec
+spec_list_semigroup =
+  describe "SemiGroup for Lists" $ do
+    it "close on list addition using length homomorphism" $
+      property (isHomorphism (length :: [Int] -> Int))
+    it "validate associative laws" $
+      property (isAssociative :: [Int] -> [Int] -> [Int] -> Bool)
 
-spec_monoid :: Spec
-spec_monoid = describe "Monoid for Lists" $ do
-  it "have commutative neutral element" $
-    property $ \xs -> (xs::[Int]) |+| zero == zero |+| xs
+spec_list_monoid :: Spec
+spec_list_monoid = describe "Monoid for Lists" $ do
   it "have neutral element" $
-    property $ \xs -> (xs::[Int]) |+| zero == xs
+    property (hasZero :: [Int] -> Bool)
 
+{-|
+  Set of Semigroup laws
+-}
+isHomorphism :: (Algebras.SemiGroup a, Algebras.SemiGroup b, Eq b) => (a -> b) -> a -> a -> Bool
+isHomorphism h x y = h x |+| h y == h (x |+| y)
 
+isAssociative :: (Algebras.SemiGroup a, Eq a) => a -> a -> a -> Bool
+isAssociative xs ys zs =
+      (xs |+| ys) |+| zs == xs |+| (ys |+| zs) &&
+        xs |+| (ys |+| zs) == xs |+| ys |+| zs
 
+{-|
+  Additional Monoid laws
+-}
+hasZero :: (Algebras.Monoid a, Eq a) => a -> Bool
+hasZero a = zero |+| a == a |+| zero && zero |+| a == a
